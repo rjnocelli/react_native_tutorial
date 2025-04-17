a3ab641a73d229127d2c2dd62bfe1b50b83a20af
Ramiro Nocelli <rnocelli@bmat.com>
2025-04-17 23:49:22 +0200

    test Pressable component

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Pressable
+        onPress={() => {
+          console.log('Button Pressed');
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? '#ddd' : '#5200ff',
+            padding: 10,
+            borderRadius: 5,
+          },
+        ]}
+      >
+        <Text style={{ color: '#fff' }}>Press Me</Text> 
+      </Pressable>
     </View>
   );
 }
@@ -13,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
