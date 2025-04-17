b43610ba0c0cfa8bde1cae8b8b4bbee2e8b1eb64
Ramiro Nocelli <rnocelli@bmat.com>
2025-04-18 00:45:58 +0200

    render some dummy data

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { getLatestGames } from "./lib/metacritic";
 
 export default function App() {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Pressable
-        onPress={() => {
-          console.log('Button Pressed');
-        }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? '#ddd' : '#5200ff',
-            padding: 10,
-            borderRadius: 5,
-          },
-        ]}
-      >
-        <Text style={{ color: '#fff' }}>Press Me</Text> 
-      </Pressable>
+      {games.map((game) => (
+        <View key={game.slug} style={styles.card}>
+          {/* <Image source={{ uri: game.image }} style={styles.image}></Image> */}
+          <Text style={styles.title}>{game.title}</Text>
+          <Text style={styles.description}>{game.description}</Text>
+          <Text style={styles.score}>{game.score}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -26,8 +29,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {},
+  image: { width: 107, height: 147, borderRadius: 10 },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    color: "#fff",
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  score: {
+    color: "#fff",
+    fontSize: 16,
+    marginBottom: 10,
   },
 });
